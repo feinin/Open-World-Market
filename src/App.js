e041a9e1457d3c04e0c213fb51c3ecb3dffa4d7d
feinin <kongkenan@gmail.com>
2024-05-29 11:38:56 -0700

    pages/routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import ShopCategory from './Pages/ShopCategory';
+import LoginSignUp from './Pages/LoginSignUp';
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/mens' element={<ShopCategory category="mens"/>}/>
+        <Route path='/woman' element={<ShopCategory category="woman"/>}/>
+        <Route path='/kids' element={<ShopCategory category="kids"/>}/>
+        <Route path="product" element={<Product/>}>
+          <Route path=':productID' element={<Product/>}/>
+        </Route>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={LoginSignUp/>}/>
+      </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
+import './Navbar.css'
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
 
 const Navbar = () => {
+
+    const [menu,setMenu] = useState("shop");
+
     return (
-        <div>
-            
+    <div className="navbar">
+     <div className="nav-logo">
+        <img src={logo} alt="" />
+            <p>Open World Market</p>
         </div>
+        <ul className="nav-menu">
+            <li onClick={()=>{setMenu("shop")}}><Link to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("mens")}}><Link to='/mens'>Men</Link>{menu==="mens"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("woman")}}><Link to='/woman'>Women</Link>{menu==="woman"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("kids")}}><Link to='/kids'>Kids</Link>{menu==="kids"?<hr/>:<></>}</li>
+        </ul>
+    <div className="nav-login-cart">
+            <Link to='login'><button>Login</button></Link>
+            <Link to='cart'><img src={cart_icon} alt=""/></Link>
+            <div className="nav-cart-count">0</div>
+        </div>
+    </div>
     )
 }
 
